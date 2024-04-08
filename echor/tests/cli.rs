8baf4f5e4d17c2e932126af0d446dbceae922fd3
kakts <kakts.git@gmail.com>
2024-04-09 00:15:22 +0900

    2.3.2: add test cases for echor.

--- echor/tests/cli.rs
+++ echor/tests/cli.rs
@@ -1,5 +1,6 @@
 use assert_cmd::Command;
 use predicates::prelude::*;
+use std::fs;
 
 #[test]
 fn dies_no_args() {
@@ -7,4 +8,12 @@ fn dies_no_args() {
     cmd.assert()
         .failure()
         .stderr(predicate::str::contains("USAGE"));
+}
+
+#[test]
+fn hello1() {
+    let outfile = "tests/expected/hello1.txt";
+    let expected = fs::read_to_string(outfile).unwrap();
+    let mut cmd = Command::cargo_bin("echor").unwrap();
+    cmd.arg("Hello there").assert().success().stdout(expected);
 }
