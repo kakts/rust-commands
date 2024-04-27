82eaf3d0a1cf4b899a57de7f70793f07dc5744d6
kakts <kakts.git@gmail.com>
2024-04-28 00:11:04 +0900

    3.2.4: print multiple files

--- catr/src/lib.rs
+++ catr/src/lib.rs
@@ -1,9 +1,58 @@
 use std::error::Error;
+use clap::{App, Arg};
+
+/**
+ * deriveマクロで、Debugトレイトを追加して構造体を表示できるようにする
+ * 
+ * Config構造体
+ */
+#[derive(Debug)]
+pub struct Config {
+    files: Vec<String>,
+    number_lines: bool, // 行番号を表示するかどうか
+    number_nonblank_lines: bool, // 空行以外の行番号を表示するかどうか
+}
 
 type MyResult<T> = Result<T, Box<dyn Error>>;
 
-pub fn run() -> MyResult<()> {
-    println!("Hello, world!");
+/**
+ * 引数としてConfigを受け取る
+ */
+pub fn run(config: Config) -> MyResult<()> {
+    for filename in config.files {
+        println!("{}", filename);
+    }
     Ok(())
+}
+
+/**
+ * 引数を解析し、Config構造体を返す
+ */
+pub fn get_args() -> MyResult<Config> {
+    let matches = App::new("catr")
+        .version("0.1.0")
+        .author("Hoge <hoge@example.com>")
+        .about("Rust cat")
+        .arg(
+            Arg::with_name("files")
+                .value_name("FILE")
+                .help("Input file(s)")
+                .multiple(true)
+                .default_value("-"),
+        )
+        .arg(
+            Arg::with_name("number")
+                .short("n")
+                .long("number")
+                .takes_value(false)
+                .help("Number all output lines"),
+        )
+        .get_matches();
+    
+    Ok(Config {
+        files: matches.values_of_lossy("files").unwrap(), // 引数の値がUTF-8として解釈させるために values_of_lossy を使う
+        number_lines: matches.is_present("number"),
+        number_nonblank_lines: matches.is_present("number_nonblank"),
+    })
 }
 
--- catr/src/main.rs
+++ catr/src/main.rs
@@ -1,5 +1,6 @@
 fn main() {
-    if let Err(e) = catr::run() {
+    // catr::get_args()の戻り値がResult型なので、エラーがあればエラーメッセージを表示して終了する
+    if let Err(e) = catr::get_args().and_then(catr::run) {
         eprintln!("{}", e);
         std::process::exit(1);
     } 
